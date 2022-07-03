eccbfbdaa970bcb49b3472709b5ea8383806d66f
Abdullah Murat Guc <abdullahmuratguc@gmail.com>
2022-07-03 16:30:03 +0300

    Kotlin dsl added.

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,36 @@
+
+/**
+ * Created by Murat
+ */
+object Libs {
+
+    const val coreKtx = "androidx.core:core-ktx:" + Versions.coreKtx
+    const val appCompat = "androidx.appcompat:appcompat:" + Versions.appCompat
+    const val material = "com.google.android.material:material:" + Versions.materialVersion
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:" + Versions.constraintVersion
+    const val lifecycleLivedata = "androidx.lifecycle:lifecycle-livedata-ktx:" + Versions.lifecycleVersion
+    const val lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:" + Versions.lifecycleVersion
+    const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:" + Versions.lifecycleVersion
+    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:" + Versions.navigationVersion
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:" + Versions.navigationVersion
+
+    const val pieChart = "com.github.PhilJay:MPAndroidChart:"+ Versions.pieChartVersion
+
+    const val roomRuntime = "androidx.room:room-runtime:" + Versions.roomVersion
+    const val roomCompiler = "androidx.room:room-compiler:" + Versions.roomVersion
+    const val roomKtx = "androidx.room:room-ktx:" + Versions.roomVersion
+
+    const val hilt = "com.google.dagger:hilt-android:" + Versions.hiltVersion
+    const val hiltCompiler =  "com.google.dagger:hilt-compiler:" + Versions.hiltVersion
+    const val hiltViewModel =  "androidx.hilt:hilt-lifecycle-viewmodel:" + Versions.hiltViewModelVersion
+    const val hiltAndroidCompiler =  "androidx.hilt:hilt-compiler:" + Versions.hiltAndroidCompilerVersion
+
+    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:" + Versions.coroutinesVersion
+    const val coroutinesAndroid =  "org.jetbrains.kotlinx:kotlinx-coroutines-android:" + Versions.coroutinesVersion
+
+    // Coroutine Lifecycle Scopes
+
+    const val jUnit = "junit:junit:" + Versions.jUnitVersion
+    const val jUnitExt = "androidx.test.ext:junit:" + Versions.jUnitExtVersion
+    const val espresso = "androidx.test.espresso:espresso-core:" + Versions.espressoVersion
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,25 @@
+/**
+* Created by Murat
+*/
+object Versions {
+    const val roomVersion = "2.4.2"
+
+    const val coreKtx = "1.8.0"
+    const val appCompat = "1.4.2"
+    const val materialVersion  = "1.6.1"
+    const val constraintVersion = "2.1.4"
+    const val lifecycleVersion = "2.5.0"
+    const val navigationVersion = "2.5.0"
+
+    const val pieChartVersion = "3.1.0"
+
+    const val hiltVersion = "2.42"
+    const val hiltViewModelVersion = "1.0.0-alpha03"
+    const val hiltAndroidCompilerVersion = "1.0.0"
+
+    const val coroutinesVersion = "1.6.1"
+
+    const val jUnitVersion = "4.13.2"
+    const val jUnitExtVersion = "1.1.3"
+    const val espressoVersion = "3.4.0"
+}
